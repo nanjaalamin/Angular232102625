fe9dbcef83b2c56c397ec9ffd2d93e60e7d2ec16
NANJA ALAMIN <alaminnanja@gmail.com>
2025-10-26 19:42:18 +0700

    first comment

---
+++ src/app/admin/admin.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { Sidebar } from "../sidebar/sidebar";
+import { Content } from "../content/content";
+import { Footer } from "../footer/footer";
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  imports: [Header, Sidebar, Content, Footer, RouterModule],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class Admin {      
+
+}

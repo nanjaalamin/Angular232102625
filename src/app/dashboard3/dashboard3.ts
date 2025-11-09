202956a180a62d14bb3b2821128c06b8b6207898
NANJA ALAMIN <alaminnanja@gmail.com>
2025-11-09 20:21:58 +0700

    init

--- src/app/dashboard3/dashboard3.ts
+++ src/app/dashboard3/dashboard3.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Footer } from "../footer/footer";
+import { Header } from "../header/header";
+import { Sidebar } from "../sidebar/sidebar";
 
 @Component({
   selector: 'app-dashboard3',
-  imports: [],
+  imports: [RouterModule, Footer, Header, Sidebar],
   templateUrl: './dashboard3.html',
   styleUrl: './dashboard3.css',
 })
